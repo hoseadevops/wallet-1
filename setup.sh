d6a37026411135eb11d15885702d42a7ae107710
droptosrisrishti <droptosrishti@gmail.com>
2018-07-04 12:20:01 +0530

    changes for postgres

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-docker exec -it $(docker ps | grep server | awk '{ print $1 }') /bin/sh -c 'npm run db:migrate'
+docker exec -it $(docker ps | grep server | awk '{ print $1 }') /bin/bash -c 'npm run db:migrate'
 docker kill $(docker ps | grep server | awk '{ print $1 }')
 docker kill $(docker ps | grep postgres | awk '{ print $1 }')
 docker-compose up
